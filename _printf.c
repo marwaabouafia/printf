b2b3aca70f70c2134d1ae0fad0cbfc0d55375dd1
Mariam <eng.mariamnabil93@gmail.com>
2023-05-30 05:02:33 -0700

    1

--- _printf.c
+++ _printf.c
@@ -11,7 +11,7 @@ int _printf(const char *format, ...)
 {
 	int result = 0;
 	va_list ap;
-	char *x, *y;
+	char *p, *y;
 	params_t params = PARAMS_INIT;
 
 	va_start(ap, format);
@@ -20,29 +20,29 @@ int _printf(const char *format, ...)
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	for (x = (char *)format; *x; x++)
+	for (p = (char *)format; *p; p++)
 	{
 		init_params(&params, ap);
-		if (*x != '%')
+		if (*p != '%')
 		{
-			result += _putchar(*x);
+			result += _putchar(*p);
 			continue;
 		}
-		y = x;
-		x++;
-		while (get_flag(x, &params))
+		y = p;
+		p++;
+		while (get_flag(p, &params))
 		{
-			x++;
+			p++;
 		}
-		x = get_width(x, &params, ap);
-		x = get_precision(x, &params, ap);
-		if (get_modifier(x, &params))
-			x++;
-		if (!get_specifie(x))
-			result += print_from_to(y, x,
-				params.l_modifier || params.h_modifier ? x - 1 : 0);
+		p = get_width(p, &params, ap);
+		p = get_precision(p, &params, ap);
+		if (get_modifier(p, &params))
+			p++;
+		if (!get_specifie(p))
+			result += print_from_to(y, p,
+				params.l_modifier || params.h_modifier ? p - 1 : 0);
 		else
-			result += get_print_func(x, ap, &params);
+			result += get_print_func(p, ap, &params);
 	}
 	_putchar(BUF_FLUSH);
 	va_end(ap);
