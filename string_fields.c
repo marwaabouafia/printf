7ffde05220d88df1ad599e7a3224578adb2fe84d
Mariam <eng.mariamnabil93@gmail.com>
2023-05-30 07:22:29 -0700

    1

--- string_fields.c
+++ string_fields.c
@@ -1,3 +1,5 @@
+#include <limits.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -8,24 +10,25 @@
  *
  * Return: new pointer
  */
+
 char *get_precision(char *p, params_t *params, va_list ap)
 {
-	int d = 0;
+	int y = 0;
 
 	if (*p != '.')
 		return (p);
 	p++;
 	if (*p == '*')
 	{
-		d = va_arg(ap, int);
+		y = va_arg(ap, int);
 		p++;
 	}
 	else
 	{
 		while (_isdigit(*p))
-			d = d * 10 + (*p++ - '0');
+			y = y * 10 + (*p++ - '0');
 	}
-	params->precision = d;
+	params->precision = y;
 	return (p);
 }
 
